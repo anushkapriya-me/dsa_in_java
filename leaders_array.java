8be37fcc1a399cb0b46cb54055266767f15dbedb
Anushka Priya <anushkapriya.me@gmail.com>
2026-05-12 17:59:52 +0530

    Leaders in an Arrays

---
+++ leaders_array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class leaders_array {
+    public static void main(String[] args) {
+        int[] arr={10,22,12,3,0,6};
+        ArrayList<Integer> leaders=new ArrayList<>();
+        int max=arr[arr.length-1];
+        leaders.add(max);
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]>max){
+                max=arr[i];
+                leaders.add(arr[i]);
+            }
+        }
+        System.out.println(leaders);
+    }
+}
